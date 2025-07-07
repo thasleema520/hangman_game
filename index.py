f13e8e20264e9f1255590eb1d440fb397f29ce45
thasleema520 <thasleema520.thasleema.shaik520@gmail.com>
2025-07-07 16:28:19 +0530

    basic game commit

--- index.py
+++ index.py
@@ -1,7 +1,43 @@
-<<<<<<< HEAD
-words = ["apple", "banana", "orange", "sweetpotato"]
+import random
+
+
+def update(alphabet, display, quizword):
+
+    for i in range(len(quizword)):
+        if quizword[i] == alphabet:
+            display[i] = alphabet
+            print(display)
+    if alphabet not in quizword:
+        print("OH no! its not there")
+    return display
+
+
+def check(res, quizword):
+    st = ""
+    for i in res:
+        st = st+i
+    if st == quizword:
+        return 1
+    else:
+        return 0
+
+
+words = ["apple", "banana", "orange", "sweetpotato", "fig"]
 quizword = random.choice(words)
-print(quizword)
-=======
-words=["apple","banana","orange","sweetpotato","fig"]
->>>>>>> 2f28e6917a78348c8874ec311e44a325efb515ee
+
+display = []
+for item in quizword:
+    display.append("_")
+print("Guess the Word:")
+print(display)
+for j in range(6):
+    print("Enter an alphabet:")
+    sub = input()
+    res = update(sub, display, quizword)
+    if check(res, quizword):
+        print("YES!! you Win")
+        break
+    else:
+        print("Try Again!")
+if check(res, quizword) == 0:
+    print("Loss")
